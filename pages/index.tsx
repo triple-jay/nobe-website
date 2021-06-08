c50df8d0856e8a75d39ec6314ade23d62e0b5c12
Andrew Osorio <ao296@cornell.edu>
2021-06-08 13:36:03 -0700

    new title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,23 +11,22 @@ export default function Home() {
         <title>NOBE Cornell </title>
         <link rel="icon" href="/nobe-cornell.png" />
       </Head>
+
       <div className={styles.container}>
         <NavigationBar />
         <div className={styles.body}>
+
           <div className={styles.nobe_cornell}>
-            <h2>NOBE&nbsp;</h2>
+            <h2>NOBE at&nbsp;</h2>
             <h2 className={styles.cornell}>Cornell</h2>
             <h3>
               {" "}
               Preparing students for success in business and engineering
             </h3>
-            <p className={styles.attribution}>
-              "Chrysler Building at Sunset, NYC" by diana_robinson is licensed
-              with CC BY-NC-ND 2.0. To view a copy of this license, visit
-              https://creativecommons.org/licenses/by-nc-nd/2.0/
-            </p>
           </div>
+
         </div>
+
       </div>
     </div>
   );
