e5d1e0ae6b40ecf7070e43f92080cfae1bdb1366
Andrew Osorio <ao296@cornell.edu>
2021-06-09 12:53:54 -0700

    application form

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -2,6 +2,9 @@ import NavigationBar from "../components/navbar"
 import Footer from '../components/footer'
 import styles from './apply.module.scss'
 
+import Link from "next/link";
+
+
 export default function Apply() {
     return (
         <div className={styles.container}>
@@ -20,11 +23,20 @@ export default function Apply() {
                     <li> ClubFest on February 13 9AM - 12PM, February 14 2PM - 5PM </li>
                     <li> Zoom Infomation Session </li>
                 </ul>
-                <button className={styles.apply_button}>Apply now!</button>
+
+                <button className={styles.apply_button}>
+                    <Link href="https://forms.gle/tJKBUGKYaFa483To8">
+                        <a>Apply now!</a>
+                    </Link>
+                </button>
+
+                {/* <button className={styles.apply_button}>Apply now!</button> */}
+
+
                 <p className={styles.p}>
                     Joining NOBE means gaining access to exclusive networking events, mentorship opportunities, and the exciting
                     opportunity to work on innovative projects in business and technology. If you have any questions,
-                    please feel free to reach out to our president, Andrew Osorio, at 
+                    please feel free to reach out to our president, Andrew Osorio, at
                     <a className={styles.mail} href="mailto:ao296@cornell.edu"> ao296@cornell.edu</a>!
                 </p>
             </div>
