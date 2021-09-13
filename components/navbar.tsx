1b5d799eb62e8a8fca2a6b1ae9a73be6ecdbef24
Jeremy Jung <jj523@cornell.edu>
2021-09-13 15:57:35 -0400

    New menu bar and about and apply page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,11 @@
 import styles from "./navbar.module.scss";
 import Link from "next/link";
+import { useState } from 'react';
 
 export default function NavigationBar() {
+
+  const [showNavList, setShowNavList] = useState(false);
+
   return (
     <div className={styles.navContainer}>
       <div className={styles.navContent}>
@@ -13,17 +17,21 @@ export default function NavigationBar() {
           />
         </a>
         <nav className={styles.navBar}>
-          <ul className={styles.navList}>
+          <img className={styles.hamburgerMenu} src="../nav-button.png" onClick={() => setShowNavList(!showNavList)} 
+          style={showNavList ? {transform: 'rotate(90deg) translateX(calc(var(--multiplier) * -10px))'} : {}}/>
+        </nav>
+      </div>
+            <ul className={styles.navList} style={showNavList ? {transform: 'scaleY(1)'} : {transform: 'scaleY(0)'}}>
             <li className={styles.navItem}>
               <Link href="/about">
                 <a>ABOUT</a>
               </Link>
             </li>
-            <li className={styles.navItem}>
+            {/* <li className={styles.navItem}>
               <Link href="/projects">
                 <a>PROJECTS</a>
               </Link>
-            </li>
+            </li> */}
             <li className={styles.navItem}>
               <Link href="/team">
                 <a>TEAM</a>
@@ -35,8 +43,6 @@ export default function NavigationBar() {
               </Link>
             </li>
           </ul>
-        </nav>
-      </div>
     </div>
   );
 }
--- pages/apply.tsx
+++ pages/apply.tsx
@@ -7,42 +7,66 @@ import Link from "next/link";
 
 export default function Apply() {
     return (
-        <div className={styles.container}>
+        <div className={styles.applyContainer}>
             <NavigationBar />
-            <div className={styles.body}>
-                <h1>Apply</h1>
-                <p className={styles.p}>
-                    Membership in NOBE is open to all who are interested! If you are interested in joining our organization, please
-                    fill out the form below! We want to get to know more about your interests, as well as the committee(s) you
-                    are interested in joining.
-                </p>
-                <p className={styles.p}>
-                    We will be present at the following events:
-                </p>
-                <ul>
-                    <li> ClubFest on February 13 9AM - 12PM, February 14 2PM - 5PM </li>
-                    <li> Zoom Infomation Session </li>
+            <div className={styles.appTextAndImg}>
+                <div className={styles.applicationTextContainer}>
+                    <h3 className={styles.appHeader}>
+                        National Organization General Application
+                    </h3>
+                    <p className={styles.appContent}>
+                        Membership in NOBE is open to all who are interested! If you are 
+                        interested in joining our organization, please fill out the form below! 
+                        We want to get to know more about your interests, as well as the committee(s) 
+                        you are interested in joining.
+                    </p>
+                    <p className={styles.appContent}>
+                        We will be present at the following events: <br/>
+                        ClubFest on February 13 9AM - 12PM, February 14 2PM - 5PM <br/>
+                        Zoom Infomation Session
+                    </p>
+                    <div className={styles.divider}></div>
+                </div>
+                <img className={styles.appImage} src="../apply-image.svg" />
+            </div>
+            <div className={styles.teamLeadsAndAppLinks}>
+                <div className={styles.teamLeadsContainer}>
+                    <div className={styles.teamLeadsContainerContent}>
+                        <h2 className={styles.teamLeadsHeader}>
+                            TEAM LEADS
+                        </h2>
+                        <h3 className={styles.teamLeadsSubHeader}>
+                            Application
+                        </h3>
+                        <div className={styles.divider}></div>
+                        <p className={styles.teamLeadsContent}>
+                            As part of a national organizaion we are hoping that everyone 
+                            can be able to gain new experiences and explore new aveneues. 
+                            For that reason, this semester we are interview for team leads.
+                        </p>
+                        <p className={styles.teamLeadsContent}>
+                            If you have an idea that you are ready interested, we would 
+                            love for you to go through our application process for team 
+                            leads. To apply, be prepared to pitch your idea, implementation 
+                            process, and companies that you would like to sponsor your project. 
+                            If there are enough resources and interest available, then we look 
+                            forward to starting new projects.
+                        </p>
+                    </div>
+                </div>
+                <ul className={styles.applicationLinks}>
+                    <li className={styles.application}>
+                        <a className={styles.appLink}>General Member Application</a>
+                    </li>
+                    <li className={styles.application}>
+                        <a className={styles.appLink}>Team Lead Application</a>
+                    </li>
+                    <li className={styles.application}>
+                        <a className={styles.appLink}>Executive Board Application</a>
+                    </li>
                 </ul>
-
-                <button className={styles.apply_button}>
-                    <Link href="https://forms.gle/tJKBUGKYaFa483To8">
-                        <a>Apply now!</a>
-                    </Link>
-                </button>
-
-                {/* <button className={styles.apply_button}>Apply now!</button> */}
-
-
-                <p className={styles.p}>
-                    Joining NOBE means gaining access to exclusive networking events, mentorship opportunities, and the exciting
-                    opportunity to work on innovative projects in business and technology. If you have any questions,
-                    please feel free to reach out to our president, Andrew Osorio, at
-                    <a className={styles.mail} href="mailto:ao296@cornell.edu"> ao296@cornell.edu</a>!
-                </p>
             </div>
-            <div className={styles.footer}>
-                <Footer />
-            </div>
+
         </div>
     );
 }
