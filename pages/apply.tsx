8b2061c156c05e27613ac4004e22f6a4247dab97
Jeremy Jung <jj523@cornell.edu>
2021-09-20 21:06:51 -0400

    Made mobile responsive and added links

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -56,13 +56,13 @@ export default function Apply() {
                 </div>
                 <ul className={styles.applicationLinks}>
                     <li className={styles.application}>
-                        <a className={styles.appLink}>General Member Application</a>
+                        <a className={styles.appLink} href="https://forms.gle/i5SisHrPNAZfvUot7">General Member Application</a>
                     </li>
                     <li className={styles.application}>
                         <a className={styles.appLink}>Team Lead Application</a>
                     </li>
                     <li className={styles.application}>
-                        <a className={styles.appLink}>Executive Board Application</a>
+                        <a className={styles.appLink} href="https://forms.gle/PZisAa589X23H67K6">Executive Board Application</a>
                     </li>
                 </ul>
             </div>
